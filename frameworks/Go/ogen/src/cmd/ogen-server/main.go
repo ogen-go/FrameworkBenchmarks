cf69af2aca576fe662be4e2898db9e8d7916a325
Aleksandr Razumov <ernado@ya.ru>
2023-03-24 22:08:37 +0300

    fix: add server header

--- frameworks/Go/ogen/src/cmd/ogen-server/main.go
+++ frameworks/Go/ogen/src/cmd/ogen-server/main.go
@@ -15,13 +15,16 @@ func run(ctx context.Context) error {
 	bindHost := flag.String("bind", ":8080", "set bind host")
 	flag.Parse()
 
-	server, err := oas.NewServer(api.Server{}, oas.WithMiddleware())
+	server, err := oas.NewServer(api.Server{})
 	if err != nil {
 		return errors.Wrap(err, "failed to create server")
 	}
 	s := &http.Server{
-		Handler: server,
-		Addr:    *bindHost,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Server", "ogen")
+			server.ServeHTTP(w, r)
+		}),
+		Addr: *bindHost,
 	}
 
 	return s.ListenAndServe()
